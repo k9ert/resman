1effae7838d2853884634335dc1d2e7847b8b322
Kim Neunert <k9ert@users.noreply.github.com>
2007-05-18 22:23:02 +0000

    mproved GUI and fixed tests

--- test/functional/courses_controller_test.rb
+++ test/functional/courses_controller_test.rb
@@ -89,4 +89,57 @@ class CoursesControllerTest < Test::Unit::TestCase
       Course.find(@first_id)
     }
   end
+
+
+  # --------------- Events-Testing -------------------------
+  fixtures :events
+  def test_new_event
+    get :new_event, :id => 1
+
+    assert_response :success
+    assert_template 'new_event'
+
+    assert_not_nil assigns(:event)
+  end
+
+  def test_create_event
+    num_events = Resman::Event.count
+
+    post :create_event, :event => {"start_time(1i)"=>"2007", "date(1i)"=>"2007", "start_time(2i)"=>"5", "date(2i)"=>"5", "date(3i)"=>"16", "start_time(3i)"=>"16", "start_time(4i)"=>"00", "end_time(1i)"=>"2007", "start_time(5i)"=>"17", "end_time(2i)"=>"5", "end_time(3i)"=>"16", "end_time(4i)"=>"08", "end_time(5i)"=>"17", "schedulable_id" => "1", "schedulable_type" => "Course"}
+
+    assert assigns["event"] != nil
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => 1
+
+    assert_equal num_events + 1, Event.count
+  end
+
+  def test_edit_event
+    get :edit_event, :id => @first_id
+
+    assert_response :success
+    assert_template 'edit_event'
+
+    assert_not_nil assigns(:event)
+    assert assigns(:event).valid?
+  end
+
+  def test_update_event
+    post :update, :id => @first_id
+    assert_response :redirect
+    assert_redirected_to :action => 'show'
+  end
+
+  def test_destroy_event
+    assert_nothing_raised {
+      Resman::Event.find(10)
+    }
+    post :destroy_event, :id => 10
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => 1
+
+    assert_raise(ActiveRecord::RecordNotFound) {
+      Event.find(10)
+    }
+  end
 end
